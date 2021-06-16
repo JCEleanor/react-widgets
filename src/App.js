a2c331d77620d56d6cc4e97d4e31dd415e8ed2de
JCEleanor <crcky4826@gmail.com>
2021-06-16 22:43:29 +0800

    create search component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import React from 'react'
-import Accordion from './components/Accordion'
-const dummyData = [
-  {
-    title: 'Declarative',
-    content:
-      'React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.',
-  },
-  {
-    title: 'Declarative',
-    content:
-      'React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.',
-  },
-  {
-    title: 'Declarative',
-    content:
-      'React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.',
-  },
-]
+import Search from './components/Search'
+// import Accordion from './components/Accordion'
+// const dummyData = [
+//   {
+//     title: 'Declarative',
+//     content:
+//       'React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.',
+//   },
+//   {
+//     title: 'Declarative',
+//     content:
+//       'React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.',
+//   },
+//   {
+//     title: 'Declarative',
+//     content:
+//       'React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.',
+//   },
+// ]
 
 function App() {
   return (
     <div>
-      <Accordion data={dummyData} />
+      <Search />
     </div>
   )
 }
