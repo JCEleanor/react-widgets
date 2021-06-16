3861b2407d7239e51d35caf04d2da899628e1694
JCEleanor <crcky4826@gmail.com>
2021-06-16 15:17:25 +0800

    set up accordion structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import Accordion from './components/Accordion'
+const dummyData = [
+  {
+    title: 'Declarative',
+    content:
+      'React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.',
+  },
+  {
+    title: 'Declarative',
+    content:
+      'React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.',
+  },
+  {
+    title: 'Declarative',
+    content:
+      'React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.',
+  },
+]
 
-export default () => {
-    return (
-        <div>from app.js</div>
-    )
+function App() {
+  return (
+    <div>
+      <Accordion data={dummyData} />
+    </div>
+  )
 }
+
+export default App
