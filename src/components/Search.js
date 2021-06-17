ce66d6f2cccf997157942be1b6b56939a317aff2
JCEleanor <crcky4826@gmail.com>
2021-06-18 00:10:27 +0800

    try wiki api with axios

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,21 @@
-import React, { useState } from 'react'
-const Search = () => {
-    const [term, setTerm] = useState('')
-    
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
+const Search = () => {
+  const [term, setTerm] = useState('')
+  useEffect(() => {
+    ;(async () => {
+      await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      })
+    })()
+  }, [term])
   return (
     <div className="ui form container">
       <div className="field">
