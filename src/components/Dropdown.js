5bef90b6aba52f133daca54e52eb0ac1a98143de
JCEleanor <crcky4826@gmail.com>
2021-06-18 18:09:39 +0800

    filter selected value from dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,17 +1,22 @@
 import React from 'react'
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
-  const renderOptions = options.map((option) => (
-    <div
-      key={option.value}
-      className="item"
-      onClick={() => {
-        onSelectedChange(option)
-      }}
-    >
-      {option.label}
-    </div>
-  ))
+  const renderOptions = options.map((option) => {
+    if (option.value === selected.value) {
+      return null
+    }
+    return (
+      <div
+        key={option.value}
+        className="item"
+        onClick={() => {
+          onSelectedChange(option)
+        }}
+      >
+        {option.label}
+      </div>
+    )
+  })
   return (
     <div className="ui form container">
       <div className="field">
