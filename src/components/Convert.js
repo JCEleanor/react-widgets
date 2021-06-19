e7111a681e269e95934a532d74ad52066a638bed
JCEleanor <crcky4826@gmail.com>
2021-06-19 23:51:48 +0800

    show result from google translate api

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,23 +1,32 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 //KEY only allows request from localhost:3000
 const KEY = 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
 
 const Convert = ({ language, input }) => {
+  const [translateResult, setTranslateResult] = useState('')
   useEffect(() => {
-    axios.post(
-      'https://translation.googleapis.com/language/translate/v2',
-      {},
-      {
-        params: {
-          q: input,
-          target: language.value,
-          key: KEY,
-        },
-      }
-    )
+    const translateInput = async () => {
+      const { data } = await axios.post(
+        'https://translation.googleapis.com/language/translate/v2',
+        {},
+        {
+          params: {
+            q: input,
+            target: language.value,
+            key: KEY,
+          },
+        }
+      )
+      setTranslateResult(data.data.translations[0].translatedText)
+    }
+    translateInput()
   }, [language, input])
-  return <div></div>
+  return (
+    <div>
+      <h1 className="ui header">{translateResult}</h1>
+    </div>
+  )
 }
 
 export default Convert
